1d7863584ff65ff7c88742472dfe16a8450000c9
Adam Shepherd <ashepherd@whoi.edu>
2020-07-21 16:10:08 -0400

    Update category-page.js

--- assets/js/includes/category-page.js
+++ assets/js/includes/category-page.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $('#blog-subnav a').on('click', function (event) {
-    console.log(event);
-    console.log(this);
+    window.location = $(this).attr('href');
   });
 });
