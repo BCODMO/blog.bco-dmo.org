2816dd8061088d52e8c053235f5fff1850e14765
Adam Shepherd <ashepherd@whoi.edu>
2020-07-29 20:06:36 -0400

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ $(document).ready( function(){
   var sjs = SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
-    searchResultTemplate: '<li><table class="table is-fullwidth"><tr><td><a href="{{ site.url }}{url}">{title}</a></td><td>{{ date }}</td><td>{{ category }}</td><td>{{ authors }}</td></tr></table></li>',
+    searchResultTemplate: '<li><table class="table is-fullwidth"><tr><td><a href="{{ site.url }}{url}">{title}</a></td><td>{date}</td><td>{category}</td><td>{authors}</td></tr></table></li>',
     json: '/search.json'
   });
   
