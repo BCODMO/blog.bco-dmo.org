9253934eaab8393a1980f911dc6af151d08d677c
Adam Shepherd <2117576+ashepherd@users.noreply.github.com>
2022-03-18 12:20:18 -0400

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ $(document).ready( function(){
   var sjs = SimpleJekyllSearch({
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container'),
-    searchResultTemplate: '<tr><td><a href="{{ site.url }}{url}">{title}</a></td><td>{date}</td><td>{category}</td><td>{authors}</td></tr>',
+    searchResultTemplate: '<tr><td><a href="{url}">{title}</a></td><td>{date}</td><td>{category}</td><td>{authors}</td></tr>',
     json: '/search.json'
   });
   
